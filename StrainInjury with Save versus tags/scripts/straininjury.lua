38e56e9911d61de7576f4824744bd3eba988c2da
Kelrugem <50429899+Kelrugem@users.noreply.github.com>
2019-08-04 23:36:34 +0200

    Add files via upload

---
+++ StrainInjury with Save versus tags/scripts/straininjury.lua
@@ -0,0 +1,6 @@
+function onInit()
+	-- Extension launch message
+	local msg = {sender = "", font = "emotefont"};
+    msg.text = "Strain and Injury HP Variant Rules v1.20 for FGII v3.3.8 3.5E and PFRPG rulesets.\rCopyright 2019 Smiteworks.\rBy darrenan (straininjury) and Kelrugem (tags).\r"
+	ChatManager.registerLaunchMessage(msg);	
+end
