f2df1173a5642e9a60e7525e4b812ad983576099
Kelrugem <50429899+Kelrugem@users.noreply.github.com>
2019-08-04 23:22:26 +0200

    Add files via upload

---
+++ Advanced Effects 3.5e with AoO/scripts/Fortif_Imm_Hresist.lua
@@ -0,0 +1,6 @@
+function onInit()
+	-- Extension launch message
+	local msg = {sender = "", font = "emotefont"};
+    msg.text = "FORTIF, HRESIST, ghost touch, Immediate action and more, for 3.5E and PFRPG rulesets. \rCopyright 2019 Smiteworks.\rBy darrenan (keen) and Kelrugem.\rMany new effects and nice stuff :) (see forum :P)"
+	ChatManager.registerLaunchMessage(msg);	
+end
