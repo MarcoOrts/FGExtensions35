19f26c1ec519efd73502ac573e11b56a926e99fb
Kelrugem <50429899+Kelrugem@users.noreply.github.com>
2019-08-04 23:50:44 +0200

    Delete Immediate_Action.lua

--- Immediate Action/scripts/Immediate_Action.lua
+++
@@ -1,6 +0,0 @@
-function onInit()
-	-- Extension launch message
-	local msg = {sender = "", font = "emotefont"};
-    msg.text = "Immediate action v1 for 3.5E and PFRPG rulesets. \rCopyright 2017 Smiteworks.\rBy Kelrugem.\rRefreshes the Immediate box at the end of turn."
-	ChatManager.registerLaunchMessage(msg);	
-end
