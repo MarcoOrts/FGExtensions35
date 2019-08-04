0a61c189928c60ea26916187ff6c00909b5e5226
Kelrugem <50429899+Kelrugem@users.noreply.github.com>
2019-08-04 23:39:02 +0200

    Add files via upload

---
+++ StrainInjury full OverlayPackage 3.5e/scripts/straininjury.lua
@@ -0,0 +1,6 @@
+function onInit()
+	-- Extension launch message
+	local msg = {sender = "", font = "emotefont"};
+    msg.text = "Strain and Injury HP Variant Rules with advanced effects, save versus tags, AoO tracker and save overlay v1.20 for FGII v3.3.8 3.5E and PFRPG rulesets.\rCopyright 2019 Smiteworks.\rBy darrenan (straininjury, keen) and Kelrugem (advanced effects, tags) (save overlay idea from Ken L).\r"
+	ChatManager.registerLaunchMessage(msg);	
+end
